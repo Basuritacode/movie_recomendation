cd762f8e8c31c3e5ccf6c67986facabc8848b3e5
Miguel <m.plazassuarez@gmail.com>
2023-12-09 18:22:47 -0500

    recomendation features

--- movie_recomender.py
+++ movie_recomender.py
@@ -1,8 +1,28 @@
 import pandas as pd
 
+# Parameters
+HEAD = 10
+MINIMUN_RATINGS = 100
+
+# Datasets
 movies_df = pd.read_csv("movies.csv")
 ratings_df = pd.read_csv("ratings.csv")
 
+# Inplace means that it overrides the variable
 movies_df.set_index("movieId", inplace=True)
 
-print(movies_df)
+# get ratings counts foreach movie: MovieId, Ratings counts
+movie_rating_counts = ratings_df["movieId"].value_counts()
+movies_df["ratingsCount"] = movie_rating_counts
+
+# Sort by top rated
+movies_df.sort_values("ratingsCount", ascending=False).head(HEAD)
+
+# Group by average ratings
+average_ratings = ratings_df.groupby("movieId").mean()["rating"]
+movies_df["averageRating"] = average_ratings
+
+# filtering on a minimun amount of ratings
+ratings_treshold: pd.DataFrame = movies_df.query(f" ratingsCount >= {MINIMUN_RATINGS} ").sort_values("ratingsCount")
+
+print(ratings_treshold)

2896e07f222d793d840bf886d80f50ba94c2dbf4
Miguel <m.plazassuarez@gmail.com>
2023-12-09 19:52:38 -0500

    euclidean distance between 2 users

--- movie_recomender.py
+++ movie_recomender.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import numpy
 
 # Parameters
 HEAD = 10
 MINIMUN_RATINGS = 100
+U1_ID = 1
+U2_ID = 610
+
 
 # Datasets
 movies_df = pd.read_csv("movies.csv")
@@ -25,4 +29,28 @@ movies_df["averageRating"] = average_ratings
 # filtering on a minimun amount of ratings
 ratings_treshold: pd.DataFrame = movies_df.query(f" ratingsCount >= {MINIMUN_RATINGS} ").sort_values("ratingsCount")
 
-print(ratings_treshold)
+
+# users are placed on a plane and the distance determines how similar they are
+def get_user_ratings(user_id):
+    user_ratings = ratings_df.query(f" userId == {user_id} ")
+    return user_ratings[["movieId", "rating"]].set_index("movieId")
+
+
+def get_users_distance(user_id_1,user_id_2):
+    """
+    Returns alistwith the users id and their respective distance
+    """
+    user_1 = get_user_ratings(user_id_1)
+    user_2 = get_user_ratings(user_id_2)
+
+    users_compared = user_1.join(user_2, lsuffix="_u1", rsuffix="_u2").dropna()
+
+    # calculate the distance using a library
+    user_1_filtered = users_compared["rating_u1"]
+    user_2_filtered = users_compared["rating_u2"]
+    distance  = numpy.linalg.norm(user_1_filtered - user_2_filtered)
+
+    return [user_id_1, user_id_2, distance] 
+
+
+print(get_users_distance(1,3))

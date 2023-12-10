8761eaa1cf6480c5d9a9d18317b71139ae5dbad9
Miguel <m.plazassuarez@gmail.com>
2023-12-09 22:25:10 -0500

    knn and training

--- movie_recomender.py
+++ movie_recomender.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy
+import random
 
 # Parameters
 HEAD = 10
@@ -8,6 +9,7 @@ MINIMUN_RATINGS = 100
 # Datasets
 movies_df = pd.read_csv("movies.csv")
 ratings_df = pd.read_csv("ratings.csv")
+ratings_df.drop(columns=['timestamp'], inplace=True)
 
 # Inplace means that it overrides the variable
 movies_df.set_index("movieId", inplace=True)
@@ -85,9 +87,56 @@ def recommend_movie(user_id):
 
 
 # K-Nearest Neighbors (KNN) is a supervised machine learning algorithm
+NEIGHBORS = 5
 
 def get_knn(user_id, k=NEIGHBORS):
-    pass
+    return get_top_matches(user_id).head(k)
 
 
-print(recommend_movie(9))
+def recommend_movie_with_knn(user_id, k=NEIGHBORS):
+    """
+    makes a movie recomendation based on the k nearest neighbors ratings 
+    """
+    user_ratings = get_user_ratings(user_id)
+    top_matches = get_knn(k)
+    ratings_by_index = ratings_df.set_index("userId")
+    top_match_ratings = ratings_by_index.loc[top_matches.index]
+
+    ratings_avg = top_match_ratings.groupby("movieId").mean()[["rating"]]
+    unwatched_movies = ratings_avg.drop(user_ratings.index, errors="ignore")
+    unwatched_movies = unwatched_movies.sort_values("rating", ascending=False)
+
+    return unwatched_movies.join(movies_df)
+
+
+# Training the Model
+MOVIES = 15
+MIN_MOVIEID = 1
+MAX_MOVIEID = movies_df.shape[0]
+MIN_RATING = 0.0
+MAX_RATING = 5.0
+
+watched_movies = []
+test_ratings = []
+
+for i in range(0, MOVIES):
+    random_movie = random.randint(MIN_MOVIEID, MAX_MOVIEID)
+    watched_movies.append(random_movie)
+
+    random_rating = random.randint(MIN_RATING, MAX_RATING)
+    test_ratings.append(random_rating)
+
+user_data = [list(index) for index in zip(watched_movies, test_ratings)]
+
+def create_new_user(user_data):
+    new_user_id = ratings_df["userId"].max() + 1
+    new_ratings = pd.DataFrame(user_data, columns=["movieId", "rating"])
+    new_ratings["userId"] = new_user_id
+
+    return pd.concat([ratings_df, new_ratings])
+
+
+
+print(recommend_movie_with_knn(611, 4))
+
+# print(recommend_movie_with_knn(9))
